ff0462462102810a8ff494d2016b8f2565ffb72f
Minas-lab <minas1991-10@mail.ru>
2020-04-14 20:45:25 +0400

    2

--- src/app.js
+++ src/app.js
@@ -1,3 +1,38 @@
-export const func = () => {
-    alert(test)
+export let admin = {}
+admin.index = 0;
+admin.editInpVal;
+admin.updateIndex;
+
+let root = document.getElementById("root")
+let btn = document.createElement("button")
+let inp = document.createElement("input")
+admin.inp = inp
+admin.btn = btn
+btn.innerText = "Add"
+root.appendChild(inp);
+root.appendChild(btn);
+
+admin.createDiv = function() {
+    let div = document.createElement("div");
+    admin.index++;
+    let p = document.createElement("p");
+    p.setAttribute("pName", admin.index)
+    let removeBtn = document.createElement("button");
+    let editBtn = document.createElement("button");
+    btn.after(div);
+    div.appendChild(p);
+    div.appendChild(editBtn);
+    div.appendChild(removeBtn);
+    p.innerText = admin.inp.value;
+    editBtn.innerText = "Edit";
+    removeBtn.innerText = "Remove";
+    editBtn.addEventListener("click", function() {
+        admin.editInpVal = this.parentElement.firstChild.innerText;
+        admin.inp.value = admin.editInpVal;
+        admin.updateIndex = this.parentElement.firstChild.getAttribute("pName")
+    });
+    admin.inp.value = "";
+    removeBtn.addEventListener("click", function() {
+        div.remove();
+    });
 }
--- src/index.js
+++ src/index.js
@@ -1,2 +1,31 @@
-let root = document.getElementById("root")
-root.innerHTML = "Test"
+import { admin } from "./app"
+admin.btn.addEventListener("click", handler);
+admin.inp.addEventListener("keydown", function(e) {
+    if (e.key == "Enter") {
+        handler();
+    }
+});
+
+
+
+function updateP() {
+    let ps = document.querySelectorAll("p");
+    for (let i = 0; i < ps.length; i++) {
+        if (ps[i].getAttribute("pName") == admin.updateIndex) {
+            ps[i].innerText = admin.inp.value;
+            admin.editInpVal = "";
+            admin.inp.value = "";
+            admin.updateIndex = ""
+        }
+    }
+}
+
+function handler() {
+    if (admin.inp.value) {
+        if (admin.editInpVal) {
+            updateP();
+        } else {
+            admin.createDiv();
+        }
+    }
+}

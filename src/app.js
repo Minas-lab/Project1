f1ea6b58902aed3c2e4b183ad5086a6369caf51e
Minas-lab <minas1991-10@mail.ru>
2020-04-20 12:19:47 +0400

    create edt

--- src/app.js
+++ src/app.js
@@ -1,38 +1,108 @@
-export let admin = {}
-admin.index = 0;
-admin.editInpVal;
-admin.updateIndex;
+// export let admin = {}
 
-let root = document.getElementById("root")
-let btn = document.createElement("button")
-let inp = document.createElement("input")
-admin.inp = inp
-admin.btn = btn
-btn.innerText = "Add"
-root.appendChild(inp);
-root.appendChild(btn);
+// let root = document.getElementById("root")
+// let btn = document.createElement("button")
+// let inp = document.createElement("input")
+// admin.inp = inp
+// admin.btn = btn
+// btn.innerText = "Add"
+// root.appendChild(inp);
+// root.appendChild(btn);
+// admin.createDiv = function() {
 
-admin.createDiv = function() {
-    let div = document.createElement("div");
-    admin.index++;
-    let p = document.createElement("p");
-    p.setAttribute("pName", admin.index)
-    let removeBtn = document.createElement("button");
-    let editBtn = document.createElement("button");
-    btn.after(div);
-    div.appendChild(p);
-    div.appendChild(editBtn);
-    div.appendChild(removeBtn);
-    p.innerText = admin.inp.value;
-    editBtn.innerText = "Edit";
-    removeBtn.innerText = "Remove";
-    editBtn.addEventListener("click", function() {
-        admin.editInpVal = this.parentElement.firstChild.innerText;
-        admin.inp.value = admin.editInpVal;
-        admin.updateIndex = this.parentElement.firstChild.getAttribute("pName")
-    });
-    admin.inp.value = "";
-    removeBtn.addEventListener("click", function() {
-        div.remove();
-    });
+//     let div = document.createElement("div");
+//     let p = document.createElement("p");
+//     let removeBtn = document.createElement("button");
+//     let editBtn = document.createElement("button");
+//     btn.after(div);
+//     div.appendChild(p);
+//     div.appendChild(editBtn);
+//     div.appendChild(removeBtn);
+//     p.innerText = admin.inp.value;
+//     editBtn.innerText = "Edit";
+//     removeBtn.innerText = "Remove";
+//     let inpEdit = document.createElement("input")
+//     editBtn.addEventListener("click", function() {
+//         let pEdit = this.parentElement.querySelector("p")
+//         pEdit.style.display = "none"
+//         editBtn.style.display = "none"
+//         this.parentElement.prepend(inpEdit)
+//         inpEdit.value = pEdit.innerText
+//         let saveBtn = document.createElement("button");
+//         saveBtn.innerText = "Save"
+//         div.appendChild(saveBtn)
+//         saveBtn.addEventListener("click", function() {
+//             inpEdit.style.display = "none"
+//             saveBtn.style.display = "none"
+//             pEdit.style.display = "block"
+//             editBtn.style.display = "block"
+//             pEdit.innerHTML = inpEdit.value
+//         })
+//     });
+//     admin.inp.value = "";
+//     removeBtn.addEventListener("click", function() {
+//         div.remove();
+//     });
+// }
+
+export class ToDo {
+    constructor() {
+        this.root = document.getElementById("root")
+        this.btn = document.createElement("button")
+        this.inp = document.createElement("input")
+        this.inpEdit = document.createElement("input")
+        this.drowInp()
+
+    }
+    drowInp() {
+        this.btn.innerText = "Add"
+        this.root.appendChild(this.inp);
+        this.root.appendChild(this.btn);
+        let createDiv = this.drowDiv.bind(this)
+        this.btn.addEventListener("click", createDiv)
+    }
+    drowDiv() {
+            let div = document.createElement("div");
+            let p = document.createElement("p");
+            let removeBtn = document.createElement("button");
+            let editBtn = document.createElement("button");
+            this.btn.after(div);
+            div.appendChild(p);
+            div.appendChild(editBtn);
+            div.appendChild(removeBtn);
+            p.innerText = this.inp.value;
+            editBtn.innerText = "Edit";
+            // const editBtnFunc = this.editBtnFunction.bind(this)
+            editBtn.addEventListener('click', () => {
+                let pEdit = this.parentElement.querySelector("p")
+                pEdit.style.display = "none"
+                editBtn.style.display = "none"
+                this.parentElement.prepend(this.inpEdit)
+                inpEdit.value = pEdit.innerText
+                let saveBtn = document.createElement("button");
+                saveBtn.innerText = "Save"
+                div.appendChild(saveBtn)
+            })
+            removeBtn.innerText = "Remove";
+            removeBtn.addEventListener("click", () => div.remove())
+            this.inp.value = ""
+        }
+        // editBtnFunction() {
+        //     console.log(this)
+        //     let pEdit = this.parentElement.querySelector("p")
+        //     pEdit.style.display = "none"
+        //     editBtn.style.display = "none"
+        //     this.parentElement.prepend(this.inpEdit)
+        //     inpEdit.value = pEdit.innerText
+        //     let saveBtn = document.createElement("button");
+        //     saveBtn.innerText = "Save"
+        //     div.appendChild(saveBtn)
+        // }
+    saveBtn() {
+        inpEdit.style.display = "none"
+        saveBtn.style.display = "none"
+        pEdit.style.display = "block"
+        editBtn.style.display = "block"
+        pEdit.innerHTML = inpEdit.value
+    }
 }
--- src/index.js
+++ src/index.js
@@ -1,31 +1,31 @@
-import { admin } from "./app"
-admin.btn.addEventListener("click", handler);
-admin.inp.addEventListener("keydown", function(e) {
-    if (e.key == "Enter") {
-        handler();
-    }
-});
+// import { admin } from "./app"
+// admin.btn.addEventListener("click", handler);
+// admin.inp.addEventListener("keydown", function(e) {
+//     if (e.key == "Enter") {
+//         handler();
+//     }
+// });
 
 
 
-function updateP() {
-    let ps = document.querySelectorAll("p");
-    for (let i = 0; i < ps.length; i++) {
-        if (ps[i].getAttribute("pName") == admin.updateIndex) {
-            ps[i].innerText = admin.inp.value;
-            admin.editInpVal = "";
-            admin.inp.value = "";
-            admin.updateIndex = ""
-        }
-    }
-}
+// function updateP() {
+//     let ps = document.querySelectorAll("p");
+//     for (let i = 0; i < ps.length; i++) {
+//         if (ps[i].getAttribute("pName") == admin.updateIndex) {
+//             ps[i].innerText = admin.inp.value;
+//             admin.editInpVal = "";
+//             admin.inp.value = "";
+//             admin.updateIndex = ""
+//         }
+//     }
+// // }
 
-function handler() {
-    if (admin.inp.value) {
-        if (admin.editInpVal) {
-            updateP();
-        } else {
-            admin.createDiv();
-        }
-    }
-}
+// function handler() {
+
+//     admin.createDiv();
+
+// }
+
+
+import { ToDo } from "./app"
+let toDo = new ToDo()
